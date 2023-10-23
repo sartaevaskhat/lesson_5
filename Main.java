e81d59602dc6cca8a5d51357822322388ae1c2ef
sartaevaskhat <148778671+sartaevaskhat@users.noreply.github.com>
2023-10-23 19:25:47 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    private static double x;
+    private static Object y;
+    private static double a2;
+
+    public static void main(String[] args) {
+
+
+      /* int pow_dareje = (int) Math.pow(3,6);
+        Math.pow(x,);
+        System.out.println(pow_dareje);
+        System.out.print(7*pow_dareje-3*x+6);*/
+
+
+  //      char a_sin;
+        //      System.out.print(a_sin);
+  //      int a = 23456;
+//
+ //       int frist_digit = a/10000;
+ //       int secend_digit = a/1000%10;
+ //       int third_digit = a/100%100;
+ //       int fourth_digit = a%100/10;
+ //       int fiveth_digit = a%10;
+ //       System.out.print(frist_digit+"  "+secend_digit+" "+third_digit);
+
+        float JSN = 110801120123f;
+        //01.08.2011
+        int jyl = (int) (jsn/10000000);
+        int jyl2 = jyl/10;
+        int ai =(int) (jsn/10000000)%100;
+        System.out.print("0"+ai+"."+jyl2);
+
+    }
+}
